4cf9d9f8a9995849f03a1505e2b440c7ece0338f
呂學昱 <x870381@gmail.com>
2014-06-15 11:57:15 +0800

    選取及移動模型

--- example/src/RenderManager.js
+++ example/src/RenderManager.js
@@ -1,24 +1,90 @@
-var RenderManager = (function(){
+var RenderManager = (function() {
 	var canvas3dId = "canvas3d";
 	var controls = null;
 	var camera = null;
 	var scene = null;
 	var light = null;
+	var projector = new THREE.Projector();
 	var renderer = new THREE.WebGLRenderer({
 		antialias: true,
 	});
+
+	var SELECTED = null;
+	var plane = null;
+	var offset = new THREE.Vector3();
 
 	function init(){
 		renderer.setClearColor(0xFFFFFF, 1.0);
 		document.getElementById(canvas3dId).appendChild(renderer.domElement);
 		window.addEventListener( 'resize', onWindowResize, false );
 
+		initMouseSelect();
 		initSize();
 		initScene();
+		initPlane();
 		initLight();
 		initCamera();
 		initControls();
 		animate();
+	}
+
+
+	function initMouseSelect() {
+		renderer.domElement.addEventListener('mousedown', onDocumentMouseDown, false);
+		renderer.domElement.addEventListener('mousemove', onDocumentMouseMove, false);
+		renderer.domElement.addEventListener('mouseup', onDocumentMouseUp, false);
+
+		var tmp = null;
+
+		function onDocumentMouseDown(event) {
+			var raycaster = raycastObject(event.clientX, event.clientY);
+
+			var selectableObject = [];
+			for (var i = 0; i <= scene.children.length - 1; i++) {
+				if(scene.children[i] != plane)
+					selectableObject.push(scene.children[i]);
+				
+			};
+
+			var intersects = raycaster.intersectObjects(selectableObject, true);
+			if (intersects.length > 0) {
+				console.log("yes");
+				controls.enabled = false;
+				SELECTED = intersects[ 0 ].object;
+				plane.position.copy( SELECTED.position );
+				plane.lookAt( camera.position );
+				var intersects = raycaster.intersectObject(plane);
+				offset.copy( intersects[ 0 ].point ).sub( plane.position );
+				//tmp = raycaster.ray.direction;
+			}
+		}
+
+		function onDocumentMouseMove(event) {
+			if(SELECTED) {
+				var raycaster = raycastObject(event.clientX, event.clientY);
+				/*var offset = new THREE.Vector3();
+				offset.copy(tmp);
+				offset.sub(raycaster.ray.direction);
+				tmp = raycaster.ray.direction;
+				SELECTED.position.sub(offset);*/
+				var intersects = raycaster.intersectObject( plane );
+				SELECTED.position.copy( intersects[ 0 ].point.sub( offset ) );
+				
+
+				render();
+				//var intersects = raycaster.intersectObject(SELECTED);
+				//SELECTED.position.copy(intersects[ 0 ].point);
+			}
+		}
+
+		function onDocumentMouseUp(event) {
+			if(SELECTED) {
+				
+				controls.enabled = true;
+				plane.position.copy( SELECTED.position );
+				SELECTED = null;
+			}
+		}
 	}
 
 	function initSize(){
@@ -27,8 +93,19 @@ var RenderManager = (function(){
 	}
 
 	function initScene(){
-		scene = new THREE.Scene();
+		scene = new THREE.Scene();	
 	}
+
+	function initPlane(){
+		plane = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000, 20, 20 ), new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.25, transparent: true, wireframe: true } ) );
+		//plane.visible = false;
+		scene.add( plane );
+	}
+
+	/*function updatePlane(){
+		plane.rotate();
+		camera.
+	}*/
 
 	function initLight() {
 		light = new THREE.DirectionalLight(0xffeedd);
@@ -112,8 +189,27 @@ var RenderManager = (function(){
 		render();
 	}
 
+	function raycastObject(rendererDomElementEventX, rendererDomElementEventY) {
+		var x = rendererDomElementEventX;
+		var y = rendererDomElementEventY;
+		var canvasSize = getCanvasSize();
+		x = (x / canvasSize.width) * 2 - 1;
+		y = -(y / canvasSize.height) * 2 + 1;
+		var vector = new THREE.Vector3(x, y, 0.5);
+		projector.unprojectVector(vector, camera);
+		var origin = camera.position;
+		var direction = vector.sub( camera.position ).normalize();
+		var raycaster = new THREE.Raycaster(origin, direction);
+		//--- debug view ---
+		//scene.add( new THREE.ArrowHelper(direction, origin, 50, 0x000000));
+		//render();
+		//------------------
+		return raycaster;
+	}
+
 	function cleanScene() {
 		initScene();
+		initPlane();
 		initLight();
 		render();
 	}
@@ -130,5 +226,6 @@ var RenderManager = (function(){
 		render: render,
 		cleanScene: cleanScene,
 		changeModel: changeModel,
+		raycastObject: raycastObject,
 	}
 })();
